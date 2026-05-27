27dde49c5a9ddf1b0545ee8d4e06e8ef3d4fc668
Dharshhana <dharshanaparamasivam05@gmail.com>
2026-05-27 11:02:18 +0530

    Revert "Remove Comment"
    This reverts commit 8cc107fea278b78299bc06c4684c4d60cea43f5a.

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ el.innerHTML=el.innerHTML
 
 @app.after_request
 def beautify_for_browser(response):
+    """If a browser requested this endpoint, render JSON as a styled HTML page."""
     ct = response.headers.get("Content-Type", "") or ""
     if not ct.startswith("application/json"):
         return response
